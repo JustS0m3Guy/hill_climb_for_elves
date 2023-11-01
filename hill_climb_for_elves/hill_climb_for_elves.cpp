29739cbd2344dea6f23f92525e8e08e55109f0ef
JustS0m3Guy <balazs200428@gmail.com>
2023-11-01 13:06:53 +0100

    Finished

--- hill_climb_for_elves/hill_climb_for_elves.cpp
+++ hill_climb_for_elves/hill_climb_for_elves.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 class Mountain {
 public:
-	vector<vector<char>> mountain_map; // a - z ch=97-122
+	vector<vector<char>> mountain_map; // a - z ch = 97-122
 	pair<int, int> start_coord;
 	pair<int, int> end_coord;
 	int &sy = start_coord.first;
@@ -36,6 +36,7 @@ public:
 					{
 						end_coord.first = i; // y coordinate
 						end_coord.second = j; // x coordinate
+						row[j] = '{'; // has a value of ch = 123
 					}
 					mountain_map[i].push_back(row[j]);
 				}
@@ -47,12 +48,12 @@ public:
 	{
 		vector<pair<int, int>> result;
 		// we check all possible neighbour to see if they would be out of bounds
-		if (coord.second - 1 >= 0) // at x = -1
-			if (int(mountain_map[coord.first][coord.second - 1]) <= int(mountain_map[coord.first][coord.second]) + 1)
-				result.push_back({ coord.first, coord.second - 1 });
 		if (coord.first - 1 >= 0) // at y = -1
 			if (int(mountain_map[coord.first - 1][coord.second]) <= int(mountain_map[coord.first][coord.second]) + 1)
 				result.push_back({ coord.first - 1, coord.second });
+		if (coord.second - 1 >= 0) // at x = -1
+			if (int(mountain_map[coord.first][coord.second - 1]) <= int(mountain_map[coord.first][coord.second]) + 1)
+				result.push_back({ coord.first, coord.second - 1 });
 		if (coord.second + 2 <= mountain_map[0].size()) // at vector.size < x (we add +2 because pair.second is an index and vector.size is a value)
 			if (int(mountain_map[coord.first][coord.second + 1]) <= int(mountain_map[coord.first][coord.second]) + 1)
 				result.push_back({ coord.first, coord.second + 1 });
@@ -68,7 +69,12 @@ public:
 		for (int i = 0; i < mountain_map.size(); i++)
 		{
 			for (int j = 0; j < mountain_map[i].size(); j++)
-				cout << mountain_map[i][j] << "";
+			{
+				if (mountain_map[i][j] == '{')
+					cout << 'E';
+				else
+					cout << mountain_map[i][j];
+			}
 			cout << "\n";
 		}
 		cout << "\nNumber of rows: " << mountain_map.size() << "\nNumber of colums: " << mountain_map[0].size();
@@ -92,55 +98,63 @@ public:
 		}
 		cout << "\nWe are on a(n): " << mountain_map[sy][sx] << "(" << int(mountain_map[sy][sx]) << ")\t(a - z ch=97-122)";
 	}
-	vector<vector<char>> ResultMaking(vector<vector<pair<int, int>>> route)
+	int ResultMaking(vector<vector<pair<int, int>>> route)
 	{
-		vector<vector<char>> result;
-
-		return result;
+		vector<vector<char>> result(mountain_map.size(),vector<char>(mountain_map[0].size(), '.'));
+		pair<int, int> current_from = route[end_coord.first][end_coord.second];
+		pair<int, int> current = end_coord;
+		int min_steps = 0;
+		result[end_coord.first][end_coord.second] = 'E';
+		while (current_from != make_pair(-1, -1))
+		{
+			if (current.second - 1 == current_from.second)
+				result[current_from.first][current_from.second] = '>';
+			else if (current.second + 1 == current_from.second)
+				result[current_from.first][current_from.second] = '<';
+			else if (current.first - 1 == current_from.first)
+				result[current_from.first][current_from.second] = 'V';
+			else if (current.first + 1 == current_from.first)
+				result[current_from.first][current_from.second] = '^';
+			current = current_from;
+			current_from = route[current_from.first][current_from.second];
+			min_steps++;
+		}
+		for (int i = 0; i < result.size(); i++)
+		{
+			cout << "\n";
+			for (int j = 0; j < result[i].size(); j++)
+				cout << result[i][j] << " ";
+		}
+		return min_steps;
 	}
-	vector<vector<char>> ShortestPath(pair<int, int> start_coords)
+	int ShortestPath(pair<int, int> start_coords)
 	{
-		vector<vector<int>> color;
-		vector<vector<pair<int, int>>> from;
+		vector<vector<int>> color(mountain_map.size(), vector<int>(mountain_map[0].size()));
+		vector<vector<pair<int, int>>> from(mountain_map.size(), vector<pair<int, int>>(mountain_map[0].size(), {-1,-1}));
 		int white = 0; // yet to be processed
 		int grey = 1; // is in queue
 		int black = 2; // is processed
 		bool end = false;
-		for (int i = 0; i < mountain_map.size(); i++)
-		{
-			color.push_back(vector<int>());
-			from.push_back(vector<pair<int, int>>());
-			for (int j = 0; j < mountain_map[i].size(); j++)
-			{
-				color[i].push_back(0);
-				from[i].push_back({ -1, -1 });
-			}
-		}
 		queue<pair<int, int>> todo;
 		todo.push(start_coords);
 		color[start_coords.first][start_coords.second] = grey;
-
 		while (!end && !todo.empty())
 		{
 			pair<int, int> to_process = todo.front();
 			todo.pop();
-			if (to_process == end_coord)
-			{
-				end = true;
-			}
 			color[to_process.first][to_process.second] = black;
 			vector<pair<int, int>> neigbours = Neighbour(to_process);
 			for (int i = 0; i < neigbours.size(); i++)
-			{
 				if (color[neigbours[i].first][neigbours[i].second] == white)
 				{
 					todo.push(neigbours[i]);
 					from[neigbours[i].first][neigbours[i].second] = to_process;
 					color[neigbours[i].first][neigbours[i].second] = grey;
 				}
-			}
+			if (to_process == end_coord)
+				end = true;
 		}
-		/**/
+		/** /
 		cout << "\n";
 		for (int i = 0; i < from.size(); i++)
 		{
@@ -148,8 +162,29 @@ public:
 				cout << "{" << from[i][j].first << ", " << from[i][j].second << "} ";
 			cout << "\n";
 		}
+		for (int i = 0; i < color.size(); i++)
+		{
+			for (int j = 0; j < color[i].size(); j++)
+				cout << color[i][j];
+			cout << "\n";
+		}
 		/**/
 		return ResultMaking(from);
+	}
+	void ShortestOfAllPath()
+	{
+		vector<pair<int, int>> starting_coords;
+		for (int i = 0; i < mountain_map.size(); i++)
+			for (int j = 0; j < mountain_map[i].size(); j++)
+				if (mountain_map[i][j] == 'a')
+				{
+					vector<pair<int, int>> neigbours = Neighbour({ i, j });
+					for (int k = 0; k < neigbours.size(); k++)
+						if (mountain_map[neigbours[k].first][neigbours[k].second] == 'b')
+							starting_coords.push_back({i, j});
+				}
+		for (int i = 0; i < starting_coords.size(); i++)
+			cout << ShortestPath(starting_coords[i]);
 	}
 };
 
@@ -157,7 +192,8 @@ int main()
 {
 	Mountain mountain;
 	mountain.Diagnostics();
-	mountain.ShortestPath(mountain.start_coord);
+	cout << "\n" << mountain.ShortestPath(mountain.start_coord);
+	//mountain.ShortestOfAllPath();
 }
 
 /*
@@ -168,46 +204,46 @@ acctuvwj
 abdefghi
 */
 /*
-abaaaaaccccccccccccccccccaaaaaaaaaaaaaccccaaaaaaaccccccccccccccccccccccccccccaaaaaa
-abaaaaaaccaaaacccccccccccaaaaaaaaacaaaacaaaaaaaaaacccccccccccccccccccccccccccaaaaaa
-abaaaaaacaaaaaccccccccccaaaaaaaaaaaaaaacaaaaaaaaaacccccccccccccaacccccccccccccaaaaa
-abaaaaaacaaaaaacccccccccaaaaaaaaaaaaaaccaaacaaaccccccccccccccccaacccccccccccccccaaa
-abccaaaccaaaaaacccaaaaccaaaaaaaaaaaaaccccaacaaacccccccccaacaccccacccccccccccccccaaa
-abcccccccaaaaaccccaaaacccccaaaaacccaaaccaaaaaaccccccccccaaaaccccccccccccccccccccaac
-abcccccccccaaaccccaaaacccccaaaaacccccccccaaaaaccccccccccklllllccccccccccccccccccccc
-abcccccccccccccccccaaccccccccaaccccccccaaaaaaaccccccccckklllllllcccccddccccaacccccc
-abaccccccccccccccccccccccccccaaccccccccaaaaaaaaccccccckkkklslllllcccddddddaaacccccc
-abacccccccccccccccccccccccccccccccaaaccaaaaaaaaccccccckkkssssslllllcddddddddacccccc
-abaccccccccccccccccccccccccccccccccaaaaccaaacaccccccckkksssssssslllmmmmmdddddaacccc
-abcccccccccccccccaaacccccccccccccaaaaaaccaacccccccccckkkssssusssslmmmmmmmdddddacccc
-abcccccccaaccccaaaaacccccccccccccaaaaaccccccaaaaaccckkkrssuuuussssqmmmmmmmmdddccccc
-abcccccccaaccccaaaaaacccccccaaccccaaaaacccccaaaaacckkkkrruuuuuussqqqqqqmmmmdddccccc
-abccccaaaaaaaacaaaaaacccccccaaaaccaaccaccccaaaaaacjkkkrrruuuxuuusqqqqqqqmmmmeeccccc
-abcaaaaaaaaaaacaaaaaccccccaaaaaacccccaaccccaaaaajjjjrrrrruuuxxuvvvvvvvqqqmmmeeccccc
-abcaacccaaaaccccaaaaaaacccaaaaacccacaaaccccaaaajjjjrrrrruuuxxxxvvvvvvvqqqmmeeeccccc
-abaaaaccaaaaacccccccaaaccccaaaaacaaaaaaaacccaajjjjrrrrtuuuuxxxyvyyyvvvqqqnneeeccccc
-abaaaaaaaaaaacccaaaaaaaccccaacaacaaaaaaaacccccjjjrrrttttuxxxxxyyyyyvvvqqnnneeeccccc
-abaaaaaaaccaacccaaaaaaaaacccccccccaaaaaaccccccjjjrrrtttxxxxxxxyyyyyvvvqqnnneeeccccc
-SbaaaaaacccccccccaaaaaaaaaccccccccaaaaacccccccjjjrrrtttxxxEzzzzyyyvvrrrnnneeecccccc
-abaaaaacccccccccccaaaaaaacccccccccaaaaaaccccccjjjqqqtttxxxxxyyyyyvvvrrrnnneeecccccc
-abaaacccccccccccaaaaaaaccaaccccccccccaaccaaaaajjjqqqttttxxxxyyyyyyvvrrrnnneeecccccc
-abaaacccccccccccaaaaaaaccaaacaaacccccccccaaaaajjjjqqqtttttxxyywyyyywvrrnnnfeecccccc
-abcaaacccccccaaaaaaaaaaacaaaaaaaccccccccaaaaaaciiiiqqqqtttxwyywwyywwwrrrnnfffcccccc
-abcccccccccccaaaaaaaaaaccaaaaaacccccccccaaaaaacciiiiqqqqttwwywwwwwwwwrrrnnfffcccccc
-abccccccccccccaaaaaacccaaaaaaaacccccccccaaaaaaccciiiiqqqttwwwwwswwwwrrrrnnfffcccccc
-abccccccccccccaaaaaacccaaaaaaaaacccccccccaaacccccciiiqqqtswwwwssssrrrrrroofffcccccc
-abccccccaaaaacaaaaaacccaaaaaaaaaaccccccccccccccccciiiqqqssswsssssssrrrrooofffaccccc
-abccccccaaaaacaaccaaccccccaaacaaacccccccccccccccccciiiqqssssssspoorrrooooofffaacccc
-abcccccaaaaaacccccccccccccaaacccccccccccccccccccccciiiqppssssspppooooooooffffaacccc
-abcccccaaaaaacccccccccccccaacccccccccccccccccccccccciipppppppppppoooooooffffaaccccc
-abcccccaaaaaaccccccccccccccccccccccccccccccccccccccciihppppppppgggggggggfffaaaccccc
-abccccccaaacccccccccccccccccccccccaccccccccccccccccchhhhpppppphggggggggggfaaaaccccc
-abaaaccccccccccccccccccccccaccccaaacccccccccccccccccchhhhhhhhhhgggggggggcaacccccccc
-abaaccaaaccaccccccccccccccaaacccaaacaacccaaaaacccccccchhhhhhhhhgaaccccccccccccccccc
-abaaacaaacaacccccccccaaaccaaaacaaaaaaaaccaaaaaccccccccchhhhhhaaaaacccccccccccccccca
-abaaaccaaaaaccccccccccaaacaaaaaaaacaaaaccaaaaaaccccccccccaaacccaaaacccccccccccaccca
-abcccaaaaaaccccccccccaaaaaaaaaaaaacaaaaccaaaaaaccccccccccaaaccccaaaccccccccccaaaaaa
-abcccaaaaaaaacccccccaaaaaaaaaaaaaaaaaccccaaaaaacccccccccccccccccccccccccccccccaaaaa
-abcccaacaaaaaccccccaaaaaaaaaaaaaaaaaaacccccaacccccccccccccccccccccccccccccccccaaaaa
+abccccaaaaaaacccaaaaaaaccccccccccccccccccccccccccccccccccaaaa
+abcccccaaaaaacccaaaaaaaaaaccccccccccccccccccccccccccccccaaaaa
+abccaaaaaaaaccaaaaaaaaaaaaaccccccccccccccccccccccccccccaaaaaa
+abccaaaaaaaaaaaaaaaaaaaaaaacccccccccaaaccccacccccccccccaaacaa
+abaccaaaaaaaaaaaaaaaaaacacacccccccccaaacccaaaccccccccccccccaa
+abaccccaaaaaaaaaaaaaaaacccccccccccccaaaaaaaaaccccccccccccccaa
+abaccccaacccccccccaaaaaacccccccccccccaaaaaaaacccccccccccccccc
+abcccccaaaacccccccaaaaaaccccccccijjjjjjaaaaaccccccaaccaaccccc
+abccccccaaaaacccccaaaacccccccciiijjjjjjjjjkkkkkkccaaaaaaccccc
+abcccccaaaaacccccccccccccccccciiiirrrjjjjjkkkkkkkkaaaaaaccccc
+abcccccaaaaaccccccccccccccccciiiirrrrrrjjjkkkkkkkkkaaaaaccccc
+abaaccacaaaaacccccccccccccccciiiqrrrrrrrrrrssssskkkkaaaaacccc
+abaaaaacaaccccccccccccccccccciiiqqrtuurrrrrsssssskklaaaaacccc
+abaaaaacccccccccccaaccccccccciiqqqttuuuurrssusssslllaaccccccc
+abaaaaaccccccccaaaaccccccccciiiqqqttuuuuuuuuuuusslllaaccccccc
+abaaaaaacccccccaaaaaaccccccciiiqqqttxxxuuuuuuuusslllccccccccc
+abaaaaaaccccaaccaaaaacccccchhiiqqtttxxxxuyyyyvvsslllccccccccc
+abaaacacccccaacaaaaaccccccchhhqqqqttxxxxxyyyyvvsslllccccccccc
+abaaacccccccaaaaaaaacccccchhhqqqqtttxxxxxyyyvvssqlllccccccccc
+abacccccaaaaaaaaaaccaaacchhhpqqqtttxxxxxyyyyvvqqqlllccccccccc
+SbaaacaaaaaaaaaaaacaaaaahhhhppttttxxEzzzzyyvvvqqqqlllcccccccc
+abaaaaaaacaaaaaacccaaaaahhhppptttxxxxxyyyyyyyvvqqqlllcccccccc
+abaaaaaaccaaaaaaaccaaaaahhhppptttxxxxywyyyyyyvvvqqqmmcccccccc
+abaaaaaaacaaaaaaacccaaaahhhpppsssxxwwwyyyyyyvvvvqqqmmmccccccc
+abaaaaaaaaaaaaaaacccaacahhhpppssssssswyyywwvvvvvqqqmmmccccccc
+abaaaaaaaacacaaaacccccccgggppppsssssswwywwwwvvvqqqqmmmccccccc
+abcaaacaaaccccaaaccccccccgggppppppssswwwwwrrrrrqqqmmmmccccccc
+abcaaacccccccccccccccccccgggggpppoosswwwwwrrrrrqqmmmmddcccccc
+abccaacccccccccccccccccccccgggggoooosswwwrrrnnnmmmmmddddccccc
+abccccccccccccccccccccccccccgggggooossrrrrrnnnnnmmmddddaccccc
+abaccccaacccccccccccccccccccccgggfoossrrrrnnnnndddddddaaacccc
+abaccaaaaaaccccccccccccccccccccgffooorrrrnnnneeddddddaaaacccc
+abaccaaaaaacccccccccccccccccccccfffooooonnnneeeddddaaaacccccc
+abacccaaaaaccccccccaaccaaaccccccffffoooonnneeeeccaaaaaacccccc
+abcccaaaaacccccccccaaccaaaaccccccffffoooneeeeeaccccccaacccccc
+abaccaaaaaccccccccaaaacaaaaccccccafffffeeeeeaaacccccccccccccc
+abacccccccccccccccaaaacaaacccccccccffffeeeecccccccccccccccaac
+abaaaacccccccaaaaaaaaaaaaaacccccccccfffeeeccccccccccccccccaaa
+abaaaacccccccaaaaaaaaaaaaaaccccccccccccaacccccccccccccccccaaa
+abaacccccccccaaaaaaaaaaaaaaccccccccccccaacccccccccccccccaaaaa
+abaaaccccccccccaaaaaaaaccccccccccccccccccccccccccccccccaaaaaa
 */
 // https://adventofcode.com/2022/day/12
